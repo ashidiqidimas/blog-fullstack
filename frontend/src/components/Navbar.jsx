a887bae86b8d9d69352d580b1cb4ce1748c06540
ashidiqidimas <prabowo6@gmail.com>
2022-09-05 21:08:19 +0700

    finished the layout

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,13 +1,35 @@
-import {Link} from "react-router-dom";
+import {Link, useLocation} from "react-router-dom";
 
 export default function Navbar() {
+  const path = useLocation().pathname;
+  console.log(path);
   return (
-    <div>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/register">Register</Link></li>
-        <li><Link to="/login">Log In</Link></li>
+    <nav className={"fixed top-0 z-50 w-full flex justify-between bg-slate-900 px-6 pt-4 pb-2"}>
+      <Link to="/">
+        <div className={"flex gap-2 items-center"}>
+          <img src="/BlogU-logo.png" alt="" className={"h-8 w-8"} />
+          <span className={"text-xl leading-7 font-medium text-primary-text"}>BlogU</span>
+        </div>
+      </Link>
+      <ul
+        className={`flex text-primary-text gap-2 text-lg leading-7 font-medium 
+        ${path === "/register" || path === "/login" ? "hidden" : ""}`
+        }>
+        <li><Link to="/login">
+          <div
+            className={"rounded-lg flex justify-center items-center w-[101px] border-2 border-sky-600"}>
+            Log In
+          </div>
+        </Link></li>
+        <li><Link to="/register">
+          <div
+            className={"rounded-lg flex justify-center items-center w-[101px] " +
+              "bg-sky-600 border-2 border-sky-600"
+            }>
+            Register
+          </div>
+        </Link></li>
       </ul>
-    </div>
+    </nav>
   );
 };
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,20 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import("tailwindcss").Config} */
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+    },
+    extend: {
+      colors: {
+        "primary-text": "#CBD5E1",
+        "secondary-text": "#94A3B8",
+        "tertiary-text": "#74869A",
+        "base": "#141F38",
+      },
+    },
   },
   plugins: [],
-}
+};
