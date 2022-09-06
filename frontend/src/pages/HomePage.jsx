cfb5c074d716ba2b79fdac83883c74d75e65e655
ashidiqidimas <prabowo6@gmail.com>
2022-09-06 14:18:16 +0700

    added API fetch for home page

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,18 +1,59 @@
 import FirstPost from "../components/FirstPost";
 import SecondPost from "../components/SecondPost";
+import {useEffect, useState} from "react";
+import {useNavigate} from "react-router-dom";
+import axios from "axios";
 
 export default function HomePage() {
+  const [posts, setPosts] = useState([]);
+  const navigate = useNavigate();
+
+  const fetchPosts = () => {
+    axios
+      .get("http://localhost:8000/post/")
+      .then((res) => {
+        const listPosts = res.data;
+        setPosts(listPosts);
+      })
+      .catch((e) => alert("error with " + e));
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  const handleClick = (id) => {
+    navigate(`/blogs/${id}`);
+  };
+
   return (
     <div className={"lg:container pt-[7.5rem] px-5"}>
-      <FirstPost />
-      <div className={"mt-16 flex flex-wrap"}>
-        <SecondPost />
-        <SecondPost />
-        <SecondPost />
-        <SecondPost />
-        <SecondPost />
-
-      </div>
+      {
+        posts.length === 0 ?
+          <p>Posts is still empty :(</p>
+          :
+          <>
+            <FirstPost
+              photoURL={posts[0].photo_URL}
+              createdAt={posts[0].createdAt}
+              postTitle={posts[0].post_title}
+              postBody={posts[0].post_body}
+              onClick={() => handleClick(posts[0].post_id)}
+            />
+            <div className={"mt-16 flex flex-wrap"}>
+              {posts.map((post) => (
+                <SecondPost
+                  photoURL={post.photo_URL}
+                  createdAt={post.createdAt}
+                  postTitle={post.post_title}
+                  postBody={post.post_body}
+                  onClick={() => handleClick(post.post_id)}
+                />
+              ))}
+            </div>
+            )}
+          </>
+      }
     </div>
   );
 };
